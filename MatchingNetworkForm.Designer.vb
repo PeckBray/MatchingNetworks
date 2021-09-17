a088469de109b0f1a491ac9167a34841f8accf03
Brayden Peck <brayden.peck2@gmail.com>
2021-09-17 16:04:23 -0600

    Add inputs for matching network type, load and source resistance and impeadnace values

--- MatchingNetworkForm.Designer.vb
+++ MatchingNetworkForm.Designer.vb
@@ -22,10 +22,95 @@ Partial Class MatchingNetworkForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.NetworksComboBox = New System.Windows.Forms.ComboBox()
+        Me.Start = New System.Windows.Forms.Button()
+        Me.LoadImpedanceTextBox = New System.Windows.Forms.TextBox()
+        Me.LoadResistanceTextBox = New System.Windows.Forms.TextBox()
+        Me.SourceImpedanceTextBox = New System.Windows.Forms.TextBox()
+        Me.SourceResistanceTextBox = New System.Windows.Forms.TextBox()
+        Me.ComplexLoadAndSourceCheckBox = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'NetworksComboBox
+        '
+        Me.NetworksComboBox.FormattingEnabled = True
+        Me.NetworksComboBox.Location = New System.Drawing.Point(12, 62)
+        Me.NetworksComboBox.Name = "NetworksComboBox"
+        Me.NetworksComboBox.Size = New System.Drawing.Size(121, 21)
+        Me.NetworksComboBox.TabIndex = 0
+        '
+        'Start
+        '
+        Me.Start.Location = New System.Drawing.Point(669, 373)
+        Me.Start.Name = "Start"
+        Me.Start.Size = New System.Drawing.Size(119, 65)
+        Me.Start.TabIndex = 1
+        Me.Start.Text = "Generate Matching Network"
+        Me.Start.UseVisualStyleBackColor = True
+        '
+        'LoadImpedanceTextBox
+        '
+        Me.LoadImpedanceTextBox.Location = New System.Drawing.Point(519, 105)
+        Me.LoadImpedanceTextBox.Name = "LoadImpedanceTextBox"
+        Me.LoadImpedanceTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.LoadImpedanceTextBox.TabIndex = 2
+        '
+        'LoadResistanceTextBox
+        '
+        Me.LoadResistanceTextBox.Location = New System.Drawing.Point(519, 62)
+        Me.LoadResistanceTextBox.Name = "LoadResistanceTextBox"
+        Me.LoadResistanceTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.LoadResistanceTextBox.TabIndex = 3
+        '
+        'SourceImpedanceTextBox
+        '
+        Me.SourceImpedanceTextBox.Location = New System.Drawing.Point(243, 105)
+        Me.SourceImpedanceTextBox.Name = "SourceImpedanceTextBox"
+        Me.SourceImpedanceTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.SourceImpedanceTextBox.TabIndex = 4
+        '
+        'SourceResistanceTextBox
+        '
+        Me.SourceResistanceTextBox.Location = New System.Drawing.Point(243, 63)
+        Me.SourceResistanceTextBox.Name = "SourceResistanceTextBox"
+        Me.SourceResistanceTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.SourceResistanceTextBox.TabIndex = 5
+        '
+        'ComplexLoadAndSourceCheckBox
+        '
+        Me.ComplexLoadAndSourceCheckBox.AutoSize = True
+        Me.ComplexLoadAndSourceCheckBox.Location = New System.Drawing.Point(12, 28)
+        Me.ComplexLoadAndSourceCheckBox.Name = "ComplexLoadAndSourceCheckBox"
+        Me.ComplexLoadAndSourceCheckBox.Size = New System.Drawing.Size(142, 17)
+        Me.ComplexLoadAndSourceCheckBox.TabIndex = 6
+        Me.ComplexLoadAndSourceCheckBox.Text = "Complex Load or Source"
+        Me.ComplexLoadAndSourceCheckBox.UseVisualStyleBackColor = True
+        '
+        'MatchingNetworkForm
+        '
+        Me.AcceptButton = Me.Start
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ComplexLoadAndSourceCheckBox)
+        Me.Controls.Add(Me.SourceResistanceTextBox)
+        Me.Controls.Add(Me.SourceImpedanceTextBox)
+        Me.Controls.Add(Me.LoadResistanceTextBox)
+        Me.Controls.Add(Me.LoadImpedanceTextBox)
+        Me.Controls.Add(Me.Start)
+        Me.Controls.Add(Me.NetworksComboBox)
+        Me.Name = "MatchingNetworkForm"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents NetworksComboBox As ComboBox
+    Friend WithEvents Start As Button
+    Friend WithEvents LoadImpedanceTextBox As TextBox
+    Friend WithEvents LoadResistanceTextBox As TextBox
+    Friend WithEvents SourceImpedanceTextBox As TextBox
+    Friend WithEvents SourceResistanceTextBox As TextBox
+    Friend WithEvents ComplexLoadAndSourceCheckBox As CheckBox
 End Class
--- MatchingNetworkForm.vb
+++ MatchingNetworkForm.vb
@@ -1,3 +1,33 @@
 ﻿Public Class MatchingNetworkForm
+    Private Sub MatchingNetworkForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        NetworksComboBox.Items.Add("L Network")
+        NetworksComboBox.Items.Add("Double L Network")
+        NetworksComboBox.Items.Add("Pi Network")
+        NetworksComboBox.Items.Add("T Network")
+        SourceImpedanceTextBox.Visible = False
+        LoadImpedanceTextBox.Visible = False
+    End Sub
 
+    Private Sub Start_Click(sender As Object, e As EventArgs) Handles Start.Click
+        Dim networkType As String = NetworksComboBox.SelectedItem
+        Dim sourceResistance As Decimal = NumberValidation(SourceResistanceTextBox.Text)
+        Dim sourceImpedance As Decimal = NumberValidation(SourceImpedanceTextBox.Text)
+        Dim loadResistance As Decimal = NumberValidation(LoadResistanceTextBox.Text)
+        Dim loadImpedance As Decimal = NumberValidation(SourceImpedanceTextBox.Text)
+    End Sub
+
+    Private Sub ComplexLoadAndSourceCheckBox_CheckedChanged(sender As Object, e As EventArgs) Handles ComplexLoadAndSourceCheckBox.CheckedChanged
+        LoadImpedanceTextBox.Visible = True
+        SourceImpedanceTextBox.Visible = True
+    End Sub
+
+    Function NumberValidation(number As String)
+        Dim trueNumber As Decimal
+        Try
+            trueNumber = CDec(number)
+        Catch ex As Exception
+
+        End Try
+        Return (trueNumber)
+    End Function
 End Class
