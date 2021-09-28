34b86de3f182fb08afd487bede93b10d69d5a7c7
Brayden Peck <brayden.peck2@gmail.com>
2021-09-28 08:53:08 -0600

    add math functions to solve for L matching network. Does not yet display reactance values or component values to user.

--- MatchingNetworkForm.Designer.vb
+++ MatchingNetworkForm.Designer.vb
@@ -28,13 +28,19 @@ Partial Class MatchingNetworkForm
         Me.LoadResistanceTextBox = New System.Windows.Forms.TextBox()
         Me.SourceImpedanceTextBox = New System.Windows.Forms.TextBox()
         Me.SourceResistanceTextBox = New System.Windows.Forms.TextBox()
-        Me.ComplexLoadAndSourceCheckBox = New System.Windows.Forms.CheckBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.LoadImpedanceComboBox = New System.Windows.Forms.ComboBox()
+        Me.SourceImpedanceComboBox = New System.Windows.Forms.ComboBox()
         Me.SuspendLayout()
         '
         'NetworksComboBox
         '
         Me.NetworksComboBox.FormattingEnabled = True
-        Me.NetworksComboBox.Location = New System.Drawing.Point(12, 62)
+        Me.NetworksComboBox.Location = New System.Drawing.Point(12, 64)
         Me.NetworksComboBox.Name = "NetworksComboBox"
         Me.NetworksComboBox.Size = New System.Drawing.Size(121, 21)
         Me.NetworksComboBox.TabIndex = 0
@@ -54,6 +60,7 @@ Partial Class MatchingNetworkForm
         Me.LoadImpedanceTextBox.Name = "LoadImpedanceTextBox"
         Me.LoadImpedanceTextBox.Size = New System.Drawing.Size(100, 20)
         Me.LoadImpedanceTextBox.TabIndex = 2
+        Me.LoadImpedanceTextBox.Text = "0"
         '
         'LoadResistanceTextBox
         '
@@ -68,6 +75,7 @@ Partial Class MatchingNetworkForm
         Me.SourceImpedanceTextBox.Name = "SourceImpedanceTextBox"
         Me.SourceImpedanceTextBox.Size = New System.Drawing.Size(100, 20)
         Me.SourceImpedanceTextBox.TabIndex = 4
+        Me.SourceImpedanceTextBox.Text = "0"
         '
         'SourceResistanceTextBox
         '
@@ -76,15 +84,66 @@ Partial Class MatchingNetworkForm
         Me.SourceResistanceTextBox.Size = New System.Drawing.Size(100, 20)
         Me.SourceResistanceTextBox.TabIndex = 5
         '
-        'ComplexLoadAndSourceCheckBox
+        'Label1
         '
-        Me.ComplexLoadAndSourceCheckBox.AutoSize = True
-        Me.ComplexLoadAndSourceCheckBox.Location = New System.Drawing.Point(12, 28)
-        Me.ComplexLoadAndSourceCheckBox.Name = "ComplexLoadAndSourceCheckBox"
-        Me.ComplexLoadAndSourceCheckBox.Size = New System.Drawing.Size(142, 17)
-        Me.ComplexLoadAndSourceCheckBox.TabIndex = 6
-        Me.ComplexLoadAndSourceCheckBox.Text = "Complex Load or Source"
-        Me.ComplexLoadAndSourceCheckBox.UseVisualStyleBackColor = True
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 48)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(121, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Matching Network Type"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(240, 48)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(97, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Source Resistance"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(516, 46)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(87, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Load Resistance"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(240, 89)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(97, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Source Impedance"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(516, 89)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(87, 13)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Load Impedance"
+        '
+        'LoadImpedanceComboBox
+        '
+        Me.LoadImpedanceComboBox.FormattingEnabled = True
+        Me.LoadImpedanceComboBox.Location = New System.Drawing.Point(465, 104)
+        Me.LoadImpedanceComboBox.Name = "LoadImpedanceComboBox"
+        Me.LoadImpedanceComboBox.Size = New System.Drawing.Size(48, 21)
+        Me.LoadImpedanceComboBox.TabIndex = 12
+        '
+        'SourceImpedanceComboBox
+        '
+        Me.SourceImpedanceComboBox.FormattingEnabled = True
+        Me.SourceImpedanceComboBox.Location = New System.Drawing.Point(187, 104)
+        Me.SourceImpedanceComboBox.Name = "SourceImpedanceComboBox"
+        Me.SourceImpedanceComboBox.Size = New System.Drawing.Size(50, 21)
+        Me.SourceImpedanceComboBox.TabIndex = 13
         '
         'MatchingNetworkForm
         '
@@ -92,7 +151,13 @@ Partial Class MatchingNetworkForm
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.ComplexLoadAndSourceCheckBox)
+        Me.Controls.Add(Me.SourceImpedanceComboBox)
+        Me.Controls.Add(Me.LoadImpedanceComboBox)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.SourceResistanceTextBox)
         Me.Controls.Add(Me.SourceImpedanceTextBox)
         Me.Controls.Add(Me.LoadResistanceTextBox)
@@ -112,5 +177,11 @@ Partial Class MatchingNetworkForm
     Friend WithEvents LoadResistanceTextBox As TextBox
     Friend WithEvents SourceImpedanceTextBox As TextBox
     Friend WithEvents SourceResistanceTextBox As TextBox
-    Friend WithEvents ComplexLoadAndSourceCheckBox As CheckBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents LoadImpedanceComboBox As ComboBox
+    Friend WithEvents SourceImpedanceComboBox As ComboBox
 End Class
--- MatchingNetworkForm.vb
+++ MatchingNetworkForm.vb
@@ -1,27 +1,40 @@
-﻿Public Class MatchingNetworkForm
+﻿Option Explicit On
+Option Strict On
+Imports System.Math
+Public Class MatchingNetworkForm
     Private Sub MatchingNetworkForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         NetworksComboBox.Items.Add("L Network")
         NetworksComboBox.Items.Add("Double L Network")
         NetworksComboBox.Items.Add("Pi Network")
         NetworksComboBox.Items.Add("T Network")
-        SourceImpedanceTextBox.Visible = False
-        LoadImpedanceTextBox.Visible = False
+        SourceImpedanceComboBox.Items.Add("+J")
+        SourceImpedanceComboBox.Items.Add("-J")
+        LoadImpedanceComboBox.Items.Add("+J")
+        LoadImpedanceComboBox.Items.Add("-J")
+        NetworksComboBox.SelectedIndex = 0
     End Sub
 
     Private Sub Start_Click(sender As Object, e As EventArgs) Handles Start.Click
-        Dim networkType As String = NetworksComboBox.SelectedItem
+        Dim networkType As String = CStr(NetworksComboBox.SelectedItem)
+        Select Case networkType
+            Case "L Network"
+                LNetwork()
+            Case "Double L Network"
+
+            Case "Pi Network"
+
+            Case "T Network"
+
+            Case Else
+
+        End Select
         Dim sourceResistance As Decimal = NumberValidation(SourceResistanceTextBox.Text)
         Dim sourceImpedance As Decimal = NumberValidation(SourceImpedanceTextBox.Text)
         Dim loadResistance As Decimal = NumberValidation(LoadResistanceTextBox.Text)
         Dim loadImpedance As Decimal = NumberValidation(SourceImpedanceTextBox.Text)
     End Sub
 
-    Private Sub ComplexLoadAndSourceCheckBox_CheckedChanged(sender As Object, e As EventArgs) Handles ComplexLoadAndSourceCheckBox.CheckedChanged
-        LoadImpedanceTextBox.Visible = True
-        SourceImpedanceTextBox.Visible = True
-    End Sub
-
-    Function NumberValidation(number As String)
+    Function NumberValidation(number As String) As Decimal
         Dim trueNumber As Decimal
         Try
             trueNumber = CDec(number)
@@ -30,4 +43,29 @@
         End Try
         Return (trueNumber)
     End Function
+    Sub LNetwork()
+        Dim sourceResistance As Decimal = NumberValidation(SourceResistanceTextBox.Text)
+        Dim sourceImpedance As Decimal = NumberValidation(SourceImpedanceTextBox.Text)
+        Dim loadResistance As Decimal = NumberValidation(LoadResistanceTextBox.Text)
+        Dim loadImpedance As Decimal = NumberValidation(LoadImpedanceTextBox.Text)
+        Dim rHigh As Decimal
+        Dim rLow As Decimal
+        Dim q As Decimal
+        Dim xs As Decimal
+        Dim xp As Decimal
+        Dim seriesComponent As String
+        Dim parallelComponent As String
+        If sourceResistance > loadResistance Then
+            rHigh = sourceResistance
+            rLow = loadResistance
+        Else
+            rHigh = loadResistance
+            rLow = sourceResistance
+        End If
+        q = CDec(Sqrt((rHigh / rLow) - 1))
+        xs = q * rLow
+        xp = rHigh / q
+
+    End Sub
+
 End Class
